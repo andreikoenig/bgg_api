87641dd3588bb5977a71afa2a416827890b5a9f3
Andrei Koenig <nv1982@gmail.com>
2016-08-20 23:29:15 -0400

    get an array of most recent hot deal threads

---
+++ hotdeals.rb
@@ -0,0 +1,26 @@
+require "mechanize"
+require "pry"
+base_url = "http://www.boardgamegeek.com/xmlapi2/"
+
+bot = Mechanize.new
+response = bot.get base_url + "forum?id=10"
+threads = response.xml.xpath("//threads").first.children.reject{|node| Nokogiri::XML::Text === node}
+
+#each thread has id, subject, author, postdate, lastpostdate.
+ten_min_ago = Time.now.utc - 60*360
+new_threads = []
+
+threads.each do |thread|
+	threadtime = Time.parse(thread["postdate"]).utc
+	if threadtime >  ten_min_ago
+		link = "https://boardgamegeek.com/thread/#{thread["id"]}"
+		new_threads << [thread["subject"], link]
+	end
+end
+
+binding.pry
+
+puts "hi"
+
+
+
