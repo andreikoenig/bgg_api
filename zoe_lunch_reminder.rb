0149ddbab14d3786bee77862212b4ef91a923231
Andrei Koenig <nv1982@gmail.com>
2016-12-09 10:46:13 -0500

    add more text messages options to zoe lunch reminder

--- zoe_lunch_reminder.rb
+++ zoe_lunch_reminder.rb
@@ -13,6 +13,27 @@ module ZoeLunchReminder
     client.account.messages.create(
 			:from => from,
 			:to => marina_number,
-			:body => "Remember to give some lunch to Zoika! Love you!")
+			:body => sms_text)
+  end
+
+  def sms_text
+    all_sms = ["Remember to give some lunch to Zoika! Love you!",
+               "Zoe! Lunch! Lunch! Lunch! Woooooo!",
+               "Woof, woof! Can I have some lunch? Thanks, Zoe",
+               "Zoe wants some nom nomz",
+               "Did you have lunch yet? Zoe wants some too!",
+               "Ushki are hungry for some lunch!",
+               "What time is it? Zoe's lunch time!",
+               "Its noon. You know what that means? Lunch for you and Zoe!",
+               "Roses are red, violets are blue, it is 12 in the afternoon, Zoe wants some lunch!",
+               "Lunch eat Zoe wants, she does yes!",
+               "Zoe will give you doggie kisses if you give her some lunch!",
+               "Love you! Will love you more if you give Zoe lunch!",
+               "A fed dog is a happy dog! Make Zoe happy - give her some lunch!",
+               "Lunch is most important meal of the day! For Zoe!",
+               "Hey beautiful! Did you give Zoe lunch yet?",
+               "In Soviet Russia, lunch eats dog at noon!",
+               "Who is awesome? You are awesome, cause you give Zoe lunch!"]
+    all_sms[rand(0..16)]
   end
 end
