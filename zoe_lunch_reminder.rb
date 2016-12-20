b37a3fc1a7c7acec5d21d9a02ba25ac149902893
Andrei Koenig <nv1982@gmail.com>
2016-12-20 11:29:41 -0500

    more sms variety

--- zoe_lunch_reminder.rb
+++ zoe_lunch_reminder.rb
@@ -34,7 +34,24 @@ module ZoeLunchReminder
                "Lunch is most important meal of the day! For Zoe!",
                "Hey beautiful! Did you give Zoe lunch yet?",
                "In Soviet Russia, lunch eats dog at noon!",
-               "Who is awesome? You are awesome, cause you give Zoe lunch!"]
+               "Who is awesome? You are awesome, cause you give Zoe lunch!",
+               "Rain. Sun. Zoe is dog. Zoe want lunch.",
+               "Give me an 'L'. Give me a 'U'. Give me an 'N'. That spells LUN!!!! (like in lunch. for Zoe)",
+               "L.U.N.C.H. (I know you know what this means)",
+               "Hungry Zoe wants lovin'. And food...",
+               "What's for lunch, mommy? Woof-woof!",
+               "Zoe told me she will trade doggy kisses for some lunch",
+               "Today for lunch special, we offer wonderful portion of Natural Balance! Duck and potato family recipe!",
+               "Hey... pssttt... sorry to interrupt... lunch for Zoe!",
+               "R3m3mb3r 2feed Zoe sum lunch. Kthxbai!",
+               "if Zoe.gets(food); Zoe.mood = happy; else; Zoe.stomach = hungry && sad; end",
+               "You are so awesome, cause you rememebered Zoe's lunch!",
+               "I just wanted to tell you that you are the best wife in the world!!! (also lunch for Zoe)",
+               "Hi! Gimme lunch! Thanks, Zoe.",
+               "Hi, Zoe called. She wants her lunch.",
+               "I just got a hunch, that you forgot Zoe's lunch, Give her some food, To Put her in an amazing mood!",
+               "Zoe. Lunch. Zoe. Lunch. Zoe. Lunch."]
+
     all_sms[rand(0..16)]
   end
 end
